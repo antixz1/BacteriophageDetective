efc5beae9e16f9c20211ac6de568465d7811893b
antixz1 <99371704+antixz1@users.noreply.github.com>
2022-03-26 14:54:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 import os
 
-#assuming Phigaro and docker are installed
+def grab_datasets():
+    #Change directory to home directory
+    os.chdir(os.path.expanduser("~"))
 
+    #accessions.txt must be in your home directory with your desired genome accession numbers or bioproject accession numbers
+    #Assign accession numbers from accessions.txt to 'accession' variable (assemblies or bioprojects)
+    with open('accessions.txt','r') as f_in:
+        accession = f_in.read().strip()
+
+    #Make results directory and make it working directory
+    os.system('mkdir results')
+    os.chdir(os.path.expanduser("~/results"))
+
+    #Download genome seqeuences of given accession numbers
+    data_set_command = 'datasets download genome accession '+accession+' --filename ncbi_datasets_test.zip --exclude-genomic-cds --exclude-gff3 --exclude-protein --exclude-rna'
+    os.system(data_set_command)
+    os.system('unzip ncbi_datasets_test.zip')
+    os.chdir('ncbi_dataset/data')
+
+    #Make directory to hold all sequences
+    os.system('mkdir all_sequences')
+
+    #Move all sequences to all_sequences
+    os.system('mv GCF*/*.fna all_sequences')
+
+    #Concatenate all sequences into one fna file
+    os.chdir('all_sequences')
+    os.system('cat *.fna > assemblies.fna')
+
+grab_datasets()
+
+#assuming Phigaro and docker are installed
 def runPhigaro():
-    runPhigaro_command = 'sudo docker run -it phigaro'
+    os.chdir(os.path.expanduser("~"))
+    runPhigaro_command = 'sudo docker run -it --name phigaro_tool phigaro'
     os.system(runPhigaro_command)
 
 runPhigaro()
