5659631b1940b23ec4d500f910a77819fe12afc9
rmansoor19 <70001087+rmansoor19@users.noreply.github.com>
2022-04-13 12:47:42 -0500

    Update Progress 4:11:2022.py

--- Progress 4:11:2022.py
+++ Progress 4:11:2022.py
@@ -14,47 +14,6 @@ $ Vim main.py
 #To exit the text editor, please enter “:q” and the return command to return to your terminal screen
 
 _____________________________________________
-# To fetch files when Biopython is not working, please use the modified code below:
-# Michael Saban modified our original code to this code to help troubleshoot issues with Biopython
-
-import os
-
-#Downloads assembly sequences from accession numbers in accessions.txt
-def grab_datasets():
-    #Change directory to home directory
-    os.chdir(os.path.expanduser("~"))
-
-    #accessions.txt must be in your home directory with your desired genome accession numbers or bioproject accession numbers
-    #Assign accession numbers from accessions.txt to 'accession' variable (assemblies or bioprojects)
-    with open('accessions.txt','r') as f_in:
-        accession = f_in.read().strip()
-    if not accession:
-        print('No accession found')
-        return
-    #Make results directory and make it working directory
-    os.system('mkdir results')
-    os.chdir(os.path.expanduser("~/results"))
-
-    #Download genome seqeuences of given accession numbers
-    data_set_command = 'datasets download genome accession '+accession+' --filename ncbi_datasets_test.zip --exclude-genomic-cds --exclude-gff3 --exclude-protein --exclude-rna'
-    print('Downloading genomes from NCBI')
-    os.system(data_set_command)
-    os.system('unzip ncbi_datasets_test.zip')
-    os.chdir('ncbi_dataset/data')
-
-    #Make directory to hold all sequences
-    os.system('mkdir all_sequences')
-
-    #Move all sequences to all_sequences
-    os.system('mv GCF*/*.fna all_sequences')
-
-    #Concatenate all sequences into one fna file
-    os.chdir('all_sequences')
-    os.system('cat *.fna > assemblies.fna')
-        
-grab_datasets()
-
-#_____________________________________________
 
 # Phigaro only accepts contigs greater than 20,000bp
 #Code for 20,000 base pairs incase the “-d” command does not work:
@@ -75,4 +34,6 @@ outfile.write(longseqs + " \n" + " \n")
 
 SeqIO.write(seqs, “path+ seqs.fasta", "fasta")
 #create a new file called "Long_seqs.fasta" in fasta format that will store all contigs greater than 20,000
+__________________________________________________
+            
 
