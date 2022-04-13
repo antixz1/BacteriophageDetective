57ade29ba877a893d58e2709f0d4e853560e891b
rmansoor19 <70001087+rmansoor19@users.noreply.github.com>
2022-04-13 18:03:35 -0500

    Update tsv parsing.py

--- tsv parsing.py
+++ tsv parsing.py
@@ -4,34 +4,24 @@ from csv import writer
 #importing numpy, pandas, and csv writer
 
 df = pd.read_csv('/Users/rmansoor/Downloads/parsed_assemblies.phigaro.tsv', sep='\t', usecols = ['scaffold','vog'])
+outfile = open('Parsed_tsv.csv','w') # creating a new csv file
+outfile.write("Scaffold," + "Number of Scaffolds," + "\n")
 var = df.scaffold.unique()
     
 mydict = {} #dictionary to count occurrences
-#loop over scaffolds
+#loop over wordlist
 for i in var:
     i = i[:9]
-    #test if scaffold is already in dict, if so add to count
+    #test if word is already in dict, if so add to count
     if i in mydict:
         mydict[i] = mydict[i] + 1
-    #otherwise add scaffold to dictionary with count 1
+    #otherwise add word to dictionary with count 1
     else:
         mydict[i] = 1
 
 for k, v in mydict.items():
-    print(str(k) + ' ' + str(v) + '\n')
+    #print(str(k) + ' ' + str(v) + '\n')
+    
+    outfile.write(str(k)+ ',' +str(v)+ ',' +"\n")
+outfile.close()
 
-
-
-
-
-
-
-
-
-# # for i in var:
-# #     letters = i[:10]
-# #     print(letters)
-
-# print(df)
-# for i in df.loc(scaffold):
-#     print(i)
