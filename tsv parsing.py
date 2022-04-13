49bb6e39924787deb1b7a25dc86a1ce50e18e197
rmansoor19 <70001087+rmansoor19@users.noreply.github.com>
2022-04-13 12:48:22 -0500

    Add files via upload

---
+++ tsv parsing.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+from csv import writer
+#importing numpy, pandas, and csv writer
+
+df = pd.read_csv('/Users/rmansoor/Downloads/parsed_assemblies.phigaro.tsv', sep='\t', usecols = ['scaffold','vog'])
+var = df.scaffold.unique()
+    
+mydict = {} #dictionary to count occurrences
+#loop over wordlist
+for i in var:
+    i = i[:9]
+    #test if word is already in dict, if so add to count
+    if i in mydict:
+        mydict[i] = mydict[i] + 1
+    #otherwise add word to dictionary with count 1
+    else:
+        mydict[i] = 1
+
+for k, v in mydict.items():
+    print(str(k) + ' ' + str(v) + '\n')
+
+
+
+
+
+
+
+
+
+# # for i in var:
+# #     letters = i[:10]
+# #     print(letters)
+
+# print(df)
+# for i in df.loc(scaffold):
+#     print(i)
