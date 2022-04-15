247de93cae240f1129920bcf7b7e66d3d4651d7d
antixz1 <99371704+antixz1@users.noreply.github.com>
2022-04-15 15:31:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import csv
+import pandas
 
 
 os.chdir(os.path.expanduser("~"))
@@ -64,6 +65,28 @@ def runPhigaro():
 runPhigaro()
 ###########################################################################################################################################################################################
 
+df = pd.read_csv('/Users/rmansoor/Downloads/parsed_assemblies.phigaro.tsv', sep='\t', usecols = ['scaffold','vog'])
+outfile = open('Parsed_tsv.csv','w') # creating a new csv file
+outfile.write("Scaffold," + "Number of Scaffolds," + "\n")
+var = df.scaffold.unique()
+    
+mydict = {} #dictionary to count occurrences
+#loop over wordlist
+for i in var:
+    i = i[:9]
+    #test if word is already in dict, if so add to count
+    if i in mydict:
+        mydict[i] = mydict[i] + 1
+    #otherwise add word to dictionary with count 1
+    else:
+        mydict[i] = 1
+
+for k, v in mydict.items():
+    #print(str(k) + ' ' + str(v) + '\n')
+    
+    outfile.write(str(k)+ ',' +str(v)+ ',' +"\n")
+outfile.close()
+########################################################################################################################################################################
 def align_prophage():
     os.chdir(os.path.expanduser("~"))
     os.system('makeblastdb -in results/phigaro_output/*.phigaro.fasta -dbtype nucl -out results/phigaroblastdb/phigarodb')
